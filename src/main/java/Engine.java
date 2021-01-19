b41820a951b8304d7048ba7179609ac266b3d030
Henrike Marie Schuster <schusterh@cip.ifi.lmu.de>
2021-01-19 09:59:57 +0100

    Engine

---
+++ src/main/java/Engine.java
@@ -0,0 +1,29 @@
+import java.util.Optional;
+
+public class Engine {
+
+    // Attribute
+    private String kind;
+    private String graphic;
+    private double speed;
+
+    // Konstruktor
+    public Engine(String graphic, double speed){
+        this.kind = "engine";
+        this.graphic = graphic;
+        this.speed = speed;
+    }
+
+    // Getter
+    public String getKind() {
+        return kind;
+    }
+    public String getGraphic() {
+        return graphic;
+    }
+    public double getSpeed() {
+        return speed;
+    }
+
+
+}
