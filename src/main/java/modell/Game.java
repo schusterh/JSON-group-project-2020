91f2ab49dcbf15e48002b4c589a4f7c05519a4ae
Henrike Marie Schuster <schusterh@cip.ifi.lmu.de>
2021-02-21 18:21:03 +0100

    findTarget: gewichtete Zufallsauswahl

--- src/main/java/modell/Game.java
+++ src/main/java/modell/Game.java
@@ -2,6 +2,7 @@ package modell;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 public class Game {
 
@@ -69,5 +70,30 @@ public class Game {
 
     public HashMap<Station, HashMap<Station, Integer>> getTransportNetwork() {
         return transportNetwork;
+    }
+
+    public Factory findTarget(Factory f, String commodity){
+        HashMap<Factory, Double> possibleTargets = new HashMap<>();
+        ArrayList<Factory> possTargets = new ArrayList<>();
+        for (Factory g : this.getFactories()) {
+            g.getProductions().stream().filter(p -> p.getConsume()
+                    .isPresent()).filter(p -> p.getConsume().get()
+                    .containsKey(commodity))
+                    .forEach(p -> possibleTargets.put(g, null));
+            double weight = (double) (g.getStorage().get(commodity) - g.getCurrentStorage().get(commodity))
+                    / transportNetwork.get(f).get(g);
+            possibleTargets.put(g, weight);
+            possTargets.add(g);
+        }
+        double totalWeight = 0.0d;
+        for (Factory factory : possTargets) {
+            totalWeight += possibleTargets.get(factory);
+        }
+        int randomIndex = 0;
+        for (double r = Math.random() * totalWeight; randomIndex < possTargets.size() -1; ++randomIndex){
+            r -= possibleTargets.get(possTargets.get(randomIndex));
+            if (r <= 0.0) break;
+        }
+        return possTargets.get(randomIndex);
     }
 }
