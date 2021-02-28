a6f2e4b3cf9421daf1633b9b9e1fb682308bce23
Valentin Vinzenz Diehl <diehl@cip.ifi.lmu.de>
2021-02-28 17:46:14 +0100

    Commented DefaultTileSet.java

--- src/main/java/ui/tiles/DefaultTileSet.java
+++ src/main/java/ui/tiles/DefaultTileSet.java
@@ -19,11 +19,11 @@ public class DefaultTileSet implements TileSet {
 
     /**
      * Tileset constructor
-     * @param tileSetPath
-     * @param colCount
-     * @param rowCount
-     * @param tileWidth
-     * @param tileHeight
+     * @param tileSetPath Path to tileset graphic
+     * @param colCount number of columns in tileset
+     * @param rowCount number of rows in tileset
+     * @param tileWidth base width of tile
+     * @param tileHeight base height of tile
      */
     public DefaultTileSet(String tileSetPath, int colCount, int rowCount, int tileWidth, int tileHeight) {
         this.colCount = colCount;
@@ -38,6 +38,9 @@ public class DefaultTileSet implements TileSet {
         this.generateTiles();
     }
 
+    /**
+     * Splits the tileset in different graphics for faster on-demand-rendering
+     */
     private void generateTiles() {
         for (int y = 0; y < this.rowCount; y++) {
             for (int x = 0; x < this.colCount; x++) {
@@ -46,11 +49,20 @@ public class DefaultTileSet implements TileSet {
         }
     }
 
+    /**
+     * Returns the graphic for a specific tile
+     * @param index index of requested tile
+     * @return Image of tile
+     */
     @Override
     public Image getTile(int index) {
         return this.tileList.get(index);
     }
 
+    /**
+     * Returns the tile resolution
+     * @return Array with width at [0] and height at [1]
+     */
     @Override
     public int[] getTileResolution() {
         return new int[]{this.TILE_WIDTH, this.TILE_HEIGHT};
